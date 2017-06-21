8408193ea2d22c54577407ba372ec830eab9490c
Eduardo Verdeja <eaverdeja@hotmail.com>
2017-06-21 20:05:05 -0300

    Primeiro commit

---
+++ StockClient/src/StockClient/StockInfoImpl.java
@@ -0,0 +1,25 @@
+package StockClient;
+
+import StockMarket.*;
+
+/**
+ * Created by Yang on 21/06/2017.
+ */
+public class StockInfoImpl extends StockInfo {
+
+
+    public  StockInfoImpl(){
+        super();
+    }
+
+    public StockInfoImpl(String name, Float value){
+        super();
+        this.name = name;
+        this.value = value;
+    }
+
+    @Override
+    public String _toString() {
+        return this.name+": "+this.value;
+    }
+}
